9d7bd22eade4756e0074ffd35887a08712fd6c94
Laszlo Csontos <github@laszlocsontos.com>
2016-07-06 13:56:12 +0200

    First working version

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
 
 set -o errexit
 set -o pipefail
@@ -13,3 +12,93 @@ __base="$(basename ${__file} .sh)"
 __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
 arg1="${1:-}"
+
+LOCKFILE="/var/lock/backup.lock"
+BACKUP_LOCAL_DIR="/mnt/backup"
+BACKUP_REMOTE_DIR="192.168.1.100:/backup"
+
+VG_NAME=$(vgs --noheadings --options vg_name | tr -d " ")
+SNAPSHOT_NAME="rdiff-snapshot"
+SNAPSHOT_DIR="/mnt/${SNAPSHOT_NAME}"
+SNAPSHOT_SIZE="1024M"
+SNAPSHOT_LV_NAME="/dev/${VG_NAME}/${SNAPSHOT_NAME}"
+
+# Previous makewhatis should execute successfully:
+[ -f $LOCKFILE ] && exit 0
+
+# Upon exit do a cleanup
+function cleanup {
+  umount $BACKUP_LOCAL_DIR
+  rm -f $LOCKFILE
+  exit 255
+}
+
+trap cleanup EXIT SIGINT SIGTERM
+touch $LOCKFILE
+
+function create_snapshot {
+  MOUNTPOINT=$1
+  TYPE=$2
+  DEV=$3
+
+  mkdir -p $SNAPSHOT_DIR
+
+  if [ "${TYPE}" == "lvm" ]; then
+    echo "Snapshotting ${MOUNTPOINT}"
+    LVS=$(lvs --noheadings --options vg_name,lv_name $DEV | awk '{ print $1","$2 }')
+    IFS="," read VG_NAME LV_NAME <<< "${LVS}"
+    lvcreate --size $SNAPSHOT_SIZE --snapshot "${VG_NAME}/${LV_NAME}" --name $SNAPSHOT_NAME
+    mount -o nouuid,ro $SNAPSHOT_LV_NAME $SNAPSHOT_DIR
+  else
+    echo "Freezing ${MOUNTPOINT}"
+    fsfreeze -f $MOUNTPOINT
+    mount -o bind $MOUNTPOINT $SNAPSHOT_DIR
+  fi
+}
+
+function delete_snapshot {
+  MOUNTPOINT=$1
+  TYPE=$2
+
+  umount $SNAPSHOT_DIR
+
+  if [ "${TYPE}" == "lvm" ]; then
+    echo "Removing snapshot for ${MOUNTPOINT}"
+    lvremove $SNAPSHOT_LV_NAME
+  else
+    echo "Un-freezing ${MOUNTPOINT}"
+    fsfreeze -u $MOUNTPOINT
+  fi
+}
+
+function backup_fs {
+  MOUNTPOINT=$1
+  TYPE=$2
+  DEV=$3
+  NAME=$4
+
+  echo "Backup of file system of ${NAME} on host $(hostname) started at $(date --rfc-3339=ns)"
+  create_snapshot $MOUNTPOINT $TYPE $DEV
+
+  BACKUP_DIR="${BACKUP_LOCAL_DIR}/$(hostname)/${NAME}"
+  findmnt -nl -R $MOUNTPOINT -o TARGET | tail -n+2 | \
+    rdiff-backup --exclude-filelist-stdin --no-eas --no-acls \
+      --print-statistics -v4 $MOUNTPOINT $BACKUP_DIR
+
+  delete_snapshot $MOUNTPOINT $TYPE
+  echo "Backup of file system of ${NAME} on host $(hostname) finished at $(date --rfc-3339=ns)"
+}
+
+echo "Mounting ${BACKUP_REMOTE_DIR} under ${BACKUP_LOCAL_DIR}"
+mount -t nfs -o proto=tcp,port=2049 $BACKUP_REMOTE_DIR $BACKUP_LOCAL_DIR
+
+IFS=$'\n'
+for FILESYSTEM in \
+  $(lsblk -npr -o MOUNTPOINT,LABEL,NAME,TYPE,FSTYPE | grep -v "^[[:space:]]" | tr " " "," | grep "^\/opt");
+do
+  IFS="," read MOUNTPOINT LABEL DEV TYPE FSTYPE    <<< "${FILESYSTEM}"
+  NAME=${LABEL:-${MOUNTPOINT#/}}
+  backup_fs $MOUNTPOINT $TYPE $DEV $NAME
+done
+
+exit 0
