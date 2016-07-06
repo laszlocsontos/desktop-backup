163f7203d3b19bc6ebef0c8d8d48070bc7ca4d73
Laszlo Csontos <github@laszlocsontos.com>
2016-07-06 15:58:22 +0200

    Remove unused variables

--- backup.sh
+++ backup.sh
@@ -3,15 +3,6 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
-
-# Set magic variables for current file & dir
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-__base="$(basename ${__file} .sh)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
-
-arg1="${1:-}"
 
 LOCKFILE="/var/lock/backup.lock"
 BACKUP_LOCAL_DIR="/mnt/backup"
