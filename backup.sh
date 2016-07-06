c76ce74d315f5502b9b5b30e49e2a1eaa65a3143
Laszlo Csontos <github@laszlocsontos.com>
2016-07-06 15:54:05 +0200

    Make error handling more resilient

--- backup.sh
+++ backup.sh
@@ -18,27 +18,22 @@ BACKUP_LOCAL_DIR="/mnt/backup"
 BACKUP_LOG_DIR="${BACKUP_LOCAL_DIR}/logs"
 BACKUP_REMOTE_DIR="192.168.1.100:/backup"
 
-VG_NAME=$(vgs --noheadings --options vg_name | tr -d " ")
 SNAPSHOT_NAME="rdiff-snapshot"
 SNAPSHOT_DIR="/mnt/${SNAPSHOT_NAME}"
 SNAPSHOT_SIZE="1024M"
-SNAPSHOT_LV_NAME="/dev/${VG_NAME}/${SNAPSHOT_NAME}"
 
 # Previous makewhatis should execute successfully:
 [ -f $LOCKFILE ] && exit 0
 
-# Upon exit do a cleanup
-function cleanup {
-  umount $BACKUP_LOCAL_DIR
-  rm -f $LOCKFILE
-  exit 255
+function get_source_device {
+  MOUNTPOINT=$1
+  if [ -z "${MOUNTPOINT}" ]; then
+    echo ""
+    return 0
+  fi
+
+  echo $(findmnt -nl $MOUNTPOINT -o SOURCE)
 }
-
-trap cleanup EXIT
-trap 'echo "INTERRUPTED"' SIGINT SIGTERM
-trap 'echo "ERROR"' ERR
-
-touch $LOCKFILE
 
 function create_snapshot {
   MOUNTPOINT=$1
@@ -52,7 +47,8 @@ function create_snapshot {
     LVS=$(lvs --noheadings --options vg_name,lv_name $DEV | awk '{ print $1","$2 }')
     IFS="," read VG_NAME LV_NAME <<< "${LVS}"
     lvcreate --size $SNAPSHOT_SIZE --snapshot "${VG_NAME}/${LV_NAME}" --name $SNAPSHOT_NAME
-    mount -o nouuid,ro $SNAPSHOT_LV_NAME $SNAPSHOT_DIR
+    SNAPSHOT_DEV="/dev/${VG_NAME}/${SNAPSHOT_NAME}"
+    mount -o nouuid,ro $SNAPSHOT_DEV $SNAPSHOT_DIR
   else
     echo "Freezing ${MOUNTPOINT}"
     fsfreeze -f $MOUNTPOINT
@@ -64,15 +60,18 @@ function delete_snapshot {
   MOUNTPOINT=$1
   TYPE=$2
 
+  SNAPSHOT_DEV=$(get_source_device $SNAPSHOT_DIR)
   umount $SNAPSHOT_DIR
 
   if [ "${TYPE}" == "lvm" ]; then
     echo "Removing snapshot for ${MOUNTPOINT}"
-    lvremove $SNAPSHOT_LV_NAME
+    lvremove -f $SNAPSHOT_DEV
   else
     echo "Un-freezing ${MOUNTPOINT}"
     fsfreeze -u $MOUNTPOINT
   fi
+
+  rmdir $SNAPSHOT_DIR
 }
 
 function backup_fs {
@@ -93,9 +92,36 @@ function backup_fs {
   echo "Backup of file system of ${NAME} on host $(hostname) finished at $(date --rfc-3339=ns)"
 }
 
+# Upon exit do a cleanup
+function cleanup {
+  if [ -d "${SNAPSHOT_DIR}" ]; then
+    SNAPSHOT_DEV=$(get_source_device $SNAPSHOT_DIR)
+    if [ -n "${SNAPSHOT_DEV}" ]; then
+      IFS="," read MOUNTPOINT TYPE \
+        <<< $(lsblk -npr -o MOUNTPOINT,TYPE $SNAPSHOT_DEV | tr " " ",")
+      delete_snapshot $MOUNTPOINT $TYPE
+    fi
+  fi
+
+  # Re-open STDOUT and STDERR
+  exec 1>&0
+  exec 2>&0
+
+  umount $BACKUP_LOCAL_DIR
+  rm -f $LOCKFILE
+  exit 255
+}
+
+trap cleanup EXIT
+trap 'echo "INTERRUPTED"' SIGINT SIGTERM
+trap 'echo "ERROR"' ERR
+
+touch $LOCKFILE
+
 mount -t nfs -o proto=tcp,port=2049 $BACKUP_REMOTE_DIR $BACKUP_LOCAL_DIR
 mkdir -p $BACKUP_LOG_DIR
 
+# Redirect STDOUT and STDERR to a log file
 exec 1<>$BACKUP_LOG_DIR/backup_$(hostname)_$(date +%Y%m%d%H%M%S).log
 exec 2>&1
 
